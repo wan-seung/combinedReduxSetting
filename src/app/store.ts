27566b611c69df8a69cf10d74184afdf122d9737
wan-seung <reddht721@gmail.com>
2021-12-23 01:00:34 +0900

    store setting

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,12 +1,67 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import counterReducer from '../features/counter/counterSlice';
-import logger from 'redux-logger'
+import logger from 'redux-logger';
+import storage from 'redux-persist/lib/storage';
+// import { myCustomApiService } from './api';
+
+// const rootReducer = combineReducers({
+//   users : usersReducer,
+//   posts : postsReducer
+// })
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+// persist를 위한 기본 세팅
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+// 두번째 인자에 combine된 리듀서 넣기
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  // 바로 하단 middleware처럼 array 형태로 저장 가능하지만 추가적으로 middleware 적용시 custom해야함
+  // middleware: [thunk, logger],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      // thunk: {
+      //   extraArgument: myCustomApiService,
+      // },
+
+      // toolkit에서 제공하는 thunk
+      thunk: true,
+      // redux-persist 설정
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+      // redux-logger 설정
+    }).concat(logger),
+  //devTools prod에서만 사용안하고 나머지 사용
+  devTools: process.env.NODE_ENV !== 'production',
 });
+
+// 새로고침해도 스토어상의 값들 유지하는거 내보내기
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
